cea6a92f0e288515297371cac0bb9cb482a72c46
songzhangliang <songzhangliang@foxmail.com>
2017-08-25 14:42:35 +0800

    第六章测试

--- shiro-example-chapter6/src/test/java/com/szl/shiro/example/chapter6/test/RealmTest.java
+++ shiro-example-chapter6/src/test/java/com/szl/shiro/example/chapter6/test/RealmTest.java
@@ -8,13 +8,44 @@ import org.junit.Test;
 import com.szl.exalple.common.BaseTest;
 
 public class RealmTest {
-
+	
+	/**
+	 * 测试认证
+	 * @author SongZhangLiang
+	 * @date 2017年8月25日 下午2:39:19
+	 */
 	@Test
 	public void testLogin() {
 		String configFile = "classpath:shiro-chapter6.ini";
 		BaseTest.testBase(configFile, "admin", "123");
 		Subject subject = BaseTest.getSubject();
 		Assert.assertTrue(subject.isAuthenticated());
+	}
+	
+	/**
+	 * 测试角色
+	 * @author SongZhangLiang
+	 * @date 2017年8月25日 下午2:39:56
+	 */
+	@Test
+	public void testRole() {
+		String configFile = "classpath:shiro-chapter6.ini";
+		BaseTest.testBase(configFile, "admin", "123");
+		Subject subject = BaseTest.getSubject();
+		Assert.assertTrue(subject.hasRole("管理员"));
+	}
+	
+	/**
+	 * 测试权限
+	 * @author SongZhangLiang
+	 * @date 2017年8月25日 下午2:40:04
+	 */
+	@Test
+	public void testPermissions() {
+		String configFile = "classpath:shiro-chapter6.ini";
+		BaseTest.testBase(configFile, "admin", "123");
+		Subject subject = BaseTest.getSubject();
+		Assert.assertTrue(subject.isPermitted("index"));
 	}
 	
 	
